5108ace246456fa0f2b9f7eaf1dedb9b69235bc6
Kaua Deja <61596237+KauaDeja@users.noreply.github.com>
2024-03-15 11:37:53 -0300

    Adicionando funcoes para configurar a balanca
    Essas funcoes vao ajudar a balanca a ler o peso corretamente

--- esp32_projetct/src/calibrar.cpp
+++ esp32_projetct/src/calibrar.cpp
@@ -10,12 +10,27 @@ const int LOADCELL_SCK_PIN = 3;
 //Variavel
 HX711 escala;
 
-//Definimos o valor de serial de cada conexao com placa
+//Definimos o valor de serial de cada conexao com placa e iniciamos a balanca
 void setup (){
     Serial.begin(9600);
     escala.begin(LOADCELL_SCK_PIN, LOADCELL_DOUT_PIN);
 }
 
 void loop(){
+
+    Serial.println("Configure a balança");
+    Serial.print("Ler: \t\t");
+    Serial.println(escala.read()); //Obtem uma leitura bruta do sensor da celula
+
+    Serial.print("Ler média: \t\t");
+    Serial.print(escala.read_average(20)); //Vai mostrar a media das ultimas 20 leituras da balanca
     
+    Serial.print("Pegar valor: \t\t");
+    Serial.print(escala.get_value(5)); //Vai mostrar a media das ultimas 5 leituras da balanca menos a carga
+
+    Serial.print("Pegar unidade: \t\t");
+    Serial.print(escala.get_units(5), 1); //Mostra a média das ultimas 5 leituras menos o peso da tara dividido pelo fator de calibração
+
+    escala.set_scale(10.2); //Inserir o seu fator de calibração encontrado
+    escala.tare();
 }
